522809afa66a1172869f9e58ef4c7370aa4e9e6f
John Viega <john@zork.org>
2023-05-08 11:34:37 -0400

    fix bug in 'dump' when using default config

--- src/commands.nim
+++ src/commands.nim
@@ -237,7 +237,7 @@ proc runCmdConfDump*() =
     toDump  = defaultConfig
     argList = getArgs()
     chalk   = getSelfExtraction().getOrElse(nil)
-    extract = chalk.extract
+    extract = if chalk != nil: chalk.extract else: nil
 
   if chalk != nil and extract != nil and extract.contains("$CHALK_CONFIG"):
     toDump  = unpack[string](extract["$CHALK_CONFIG"])
