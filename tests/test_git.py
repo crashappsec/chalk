2cb7f9d312cafcf8edbbe8dd4e0db2ee978beaf0
Miroslav Shubernetskiy <miroslav@miki725.com>
2023-10-05 17:06:09 -0400

    fix(git): fixing empty git repo resulting in segfault
    code was assuming git ref file for the branch exists but its missing
    for an empty repo as there are no commits in the repo yet

--- tests/test_git.py
+++ tests/test_git.py
@@ -8,11 +8,10 @@ import pytest
 
 from .chalk.runner import Chalk
 from .conf import LS_PATH
-from .utils.dict import ANY
+from .utils.dict import ANY, MISSING
 from .utils.git import init
 
 
-@pytest.mark.parametrize("first_commit", [True, False])
 @pytest.mark.parametrize("remote", ["git@github.com:crashappsec/chalk.git", None])
 @pytest.mark.parametrize("copy_files", [[LS_PATH]], indirect=True)
 def test_repo(
@@ -20,13 +19,30 @@ def test_repo(
     chalk_copy: Chalk,
     copy_files: list[Path],
     remote: str,
-    first_commit: bool,
 ):
-    init(tmp_data_dir, remote=remote, first_commit=first_commit)
+    init(tmp_data_dir, remote=remote, first_commit=True)
     artifact = copy_files[0]
-    result = chalk_copy.insert(artifact, log_level="error")
+    result = chalk_copy.insert(artifact, log_level="trace")
     assert result.mark.has(
         BRANCH="main",
         COMMIT_ID=ANY,
         ORIGIN_URI=remote or "local",
+        VCS_DIR_WHEN_CHALKED=str(tmp_data_dir),
+    )
+
+
+@pytest.mark.parametrize("copy_files", [[LS_PATH]], indirect=True)
+def test_empty_repo(
+    tmp_data_dir: Path,
+    chalk_copy: Chalk,
+    copy_files: list[Path],
+):
+    init(tmp_data_dir, first_commit=False)
+    artifact = copy_files[0]
+    result = chalk_copy.insert(artifact, log_level="trace")
+    assert result.mark.has(
+        BRANCH=MISSING,
+        COMMIT_ID=MISSING,
+        ORIGIN_URI=MISSING,
+        VCS_DIR_WHEN_CHALKED=MISSING,
     )
