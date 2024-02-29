17a23bfd06d8bfe969cf033da6f56137d14ae0bc
ee7 <45465154+ee7@users.noreply.github.com>
2024-02-29 15:25:01 +0100

    tests(semver): move tests to test directory
    Prepare for running unit tests in CI.
    
    With this commit, the only occurrence of `isMainModule` is in
    `src/chalk.nim`.

---
+++ tests-nim/test_semver.nim
@@ -0,0 +1,45 @@
+import ../src/semver
+
+proc main() =
+  assert($(parseVersion("0.1")) == "0.1")
+  assert($(parseVersion("0.1-dev")) == "0.1-dev")
+  assert($(parseVersion("0.1.0")) == "0.1.0")
+  assert($(parseVersion("0.1.0-dev")) == "0.1.0-dev")
+
+  assert(parseVersion("0.1") == parseVersion("0.1.0"))
+  assert(parseVersion("0.1.0") == parseVersion("0.1.0"))
+  assert(not(parseVersion("0.1") == parseVersion("0.1.5")))
+  assert(not(parseVersion("0.1") == parseVersion("0.1-dev")))
+  assert(not(parseVersion("0.1.0") == parseVersion("0.1.5")))
+
+  assert(parseVersion("0.1") != parseVersion("0.1.5"))
+  assert(parseVersion("0.1") != parseVersion("0.1-dev"))
+  assert(parseVersion("0.1.0") != parseVersion("0.1.5"))
+  assert(not(parseVersion("0.1") != parseVersion("0.1")))
+  assert(not(parseVersion("0.1.0") != parseVersion("0.1")))
+
+  assert(parseVersion("0.1-dev") < parseVersion("0.1"))
+  assert(parseVersion("0.1") < parseVersion("0.1.5"))
+  assert(parseVersion("0.1.0") < parseVersion("0.1.5"))
+  assert(not(parseVersion("0.1") < parseVersion("0.1")))
+  assert(not(parseVersion("0.1") < parseVersion("0.1.0")))
+
+  assert(parseVersion("0.1-dev") <= parseVersion("0.1"))
+  assert(parseVersion("0.1") <= parseVersion("0.1.5"))
+  assert(parseVersion("0.1.0") <= parseVersion("0.1.5"))
+  assert(parseVersion("0.1") <= parseVersion("0.1"))
+  assert(parseVersion("0.1") <= parseVersion("0.1.0"))
+
+  assert(parseVersion("0.1") > parseVersion("0.1-dev"))
+  assert(parseVersion("0.1.5") > parseVersion("0.1"))
+  assert(parseVersion("0.1.5") > parseVersion("0.1.0"))
+  assert(not(parseVersion("0.1") > parseVersion("0.1")))
+  assert(not(parseVersion("0.1.0") > parseVersion("0.1")))
+
+  assert(parseVersion("0.1") >= parseVersion("0.1-dev"))
+  assert(parseVersion("0.1.5") >= parseVersion("0.1"))
+  assert(parseVersion("0.1.5") >= parseVersion("0.1.0"))
+  assert(parseVersion("0.1") >= parseVersion("0.1"))
+  assert(parseVersion("0.1.0") >= parseVersion("0.1"))
+
+main()
