fbd8c6231bee41d3e3e19a380f6a88f2ef98deb2
ee7 <45465154+ee7@users.noreply.github.com>
2024-02-29 17:00:44 +0100

    refactor(all_nim_tests): use staticListFiles over new getSortedPaths
    Refactor the staticListFiles implementation later.

--- tests-nim/all_nim_tests.nim
+++ tests-nim/all_nim_tests.nim
@@ -1,19 +1,11 @@
 import std/[algorithm, macros, os, strutils]
-
-proc getSortedPaths(dir: string, pc: PathComponent, relative: bool): seq[string] =
-  ## Returns paths in `dir` of kind `pc` in alphabetical order.
-  ##
-  ## If `relative` is `true`, the returned paths are relative to `dir`.
-  result = @[]
-  for kind, path in walkDir(dir, relative = relative):
-    if kind == pc:
-      result.add path
-  sort result
+from pkg/nimutils import staticListFiles
 
 proc getBracketedTestModules(): NimNode =
   ## Returns the AST for a `nnkBracket` containing the module names of Nim
   ## test files in this directory.
-  const files = getSortedPaths(currentSourcePath().parentDir(), pcFile, relative = true)
+  var files = currentSourcePath().parentDir().staticListFiles()
+  sort files
   result = nnkBracket.newTree()
   for f in files:
     if f.startsWith("test_") and f.endsWith(".nim") and f.len > 9:
