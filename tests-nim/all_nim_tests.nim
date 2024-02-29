82c50eac96fe215b2edb424dbd531bf543308efd
ee7 <45465154+ee7@users.noreply.github.com>
2024-02-29 16:40:07 +0100

    tests(all-nim-tests): simplify by removing scanf usage

--- tests-nim/all_nim_tests.nim
+++ tests-nim/all_nim_tests.nim
@@ -1,35 +1,23 @@
-import std/[algorithm, macros, os, parseutils, strscans, strutils]
+import std/[algorithm, macros, os, strutils]
 
-func w(s: string, start: int): int =
-  ## Returns the number of characters in `s` that are valid Nim identifier
-  ## characters, beginning from `start`.
-  ##
-  ## This is a custom matcher for `scanf`. It is similar to the standard
-  ## library's `$w` matcher, but it only skips (and does not bind).
-  s.skipWhile(IdentChars, start)
-
-proc getSortedPaths(
-    dir: string, pc: PathComponent, relative: bool, pattern: static string
-): seq[string] =
-  ## Returns paths in `dir` of kind `pc` that match the given scanf `pattern`,
-  ## in alphabetical order.
+proc getSortedPaths(dir: string, pc: PathComponent, relative: bool): seq[string] =
+  ## Returns paths in `dir` of kind `pc` in alphabetical order.
   ##
   ## If `relative` is `true`, the returned paths are relative to `dir`.
   result = @[]
   for kind, path in walkDir(dir, relative = relative):
-    if kind == pc and path.scanf(pattern):
+    if kind == pc:
       result.add path
   sort result
 
 proc getBracketedTestModules(): NimNode =
   ## Returns the AST for a `nnkBracket` containing the module names of Nim
   ## test files in this directory.
-  const files = getSortedPaths(
-    currentSourcePath().parentDir(), pcFile, relative = true, "test_$[w].nim$."
-  )
+  const files = getSortedPaths(currentSourcePath().parentDir(), pcFile, relative = true)
   result = nnkBracket.newTree()
   for f in files:
-    result.add ident(f[0 ..^ 5]) # Remove .nim file extension.
+    if f.startsWith("test_") and f.endsWith(".nim") and f.len > 9:
+      result.add ident(f[0 ..^ 5]) # Remove .nim file extension.
   expectMinLen(result, 1)
 
 macro importTestFiles() =
