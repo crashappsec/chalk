210946ee8fe9b54e83a3ed86f12557de3ac13022
ee7 <45465154+ee7@users.noreply.github.com>
2024-04-24 22:20:01 +0200

    tests(Dockerfile, entrypoint): revert changes

--- tests/functional/data/dockerfiles/valid/base/entrypoint.sh
+++ tests/functional/data/dockerfiles/valid/base/entrypoint.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 
-# if [ "$#" -ne 2 ]; then
-#     echo "Wrong number of arguments"
-#     exit 1
-# fi
+if [ "$#" -ne 1 ]; then
+    echo "Wrong number of arguments"
+    exit 1
+fi
 
-# arg=$1
-# if [ "$arg" = "true" ]; then
-#     echo "Args are correct"
-#     exit 0
-# fi
+arg=$1
+if [ "$arg" = "true" ]; then
+    echo "Args are correct"
+    exit 0
+fi
 
-# echo "Args are wrong. Expect one true arg"
-# exit 1
-echo "ignore all this"
+echo "Args are wrong. Expect one true arg"
+exit 1
