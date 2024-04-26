ca217bd77211a0a6125840fd9167e3f76340738b
ee7 <45465154+ee7@users.noreply.github.com>
2024-04-26 14:11:45 +0200

    test(fd_cache): make tests self-contained, and enable in CI (#276)
    The previous commit [1] added testament-based unit testing, but didn't
    migrate the fd_cache tests because they relied on files to be manually
    created. On `main`, we would see the error:
    
        $ nim r src/fd_cache.nim
        fatal.nim(53)            sysFatal
        Error: unhandled exception: fd_cache.nim(272, 11) `one1 != two`  [AssertionDefect]
    
    Make those tests self-contained, and migrate them to the new
    testament-based system so they now run in CI.
    
    Also prefer `doAssert` as Nim itself recommends [2], and use `{.all.}`
    to avoid adding the export marker in source files only for testing
    purposes. From the Nim 1.6.0 release post [3]:
    
        A new import syntax `import foo {.all.}` now allows importing all
        symbols (public or private) from foo. This can be useful for testing
        purposes or for more flexibility in project organization.
    
    Closes: https://github.com/crashappsec/chalk/issues/275
    
    [1] 46ffecdfff56, 2024-04-26, "ci: add testament-based unit testing"
    [2] https://nim-lang.org/2.0.0/contributing.html#writing-tests-stdlib
    [3] https://nim-lang.org/blog/2021/10/19/version-160-released.html#private-imports-and-private-field-access

---
+++ tests/unit/test_fd_cache.nim
@@ -0,0 +1,62 @@
+import std/os
+# Import non-exported `newFDCache`, which is currently used only in these tests.
+import ../../src/fd_cache {.all.}
+
+proc withCache() =
+  let
+    testCache = newFDCache(size = 2)
+    one1       = testCache.acquireFileStream("one")
+    one2       = testCache.acquireFileStream("one")
+    two        = testCache.acquireFileStream("two")
+  doAssert(one1 == one2)
+  doAssert(one1 != two)
+
+  try:
+    # should not be allowed to acquire file as one is not released
+    discard testCache.acquireFileStream("three")
+    doAssert(false)
+  except:
+    doAssert(true)
+
+  testCache.releaseFileStream(one1)
+  try:
+    # should still not be allowed to acquire file as one is not released
+    discard testCache.acquireFileStream("three")
+    doAssert(false)
+  except:
+    doAssert(true)
+
+  testCache.releaseFileStream(one2)
+  # we can finally get three as all ones have been released
+  let three      = testCache.acquireFileStream("three")
+
+  testCache.releaseFileStream(two)
+  let one3       = testCache.acquireFileStream("one")
+  doAssert(one1 != one3)
+
+  testCache.releaseFileStream(three)
+
+  testCache.withFileStream("one", mode = fmRead, strict = true):
+    doAssert(stream != nil)
+  doAssert(stream == nil)
+
+proc global() =
+  withFileStream("one", mode = fmRead, strict = true):
+    doAssert(stream != nil)
+  doAssert(stream == nil)
+
+proc main =
+  const files = ["one", "two", "three"]
+
+  for f in files:
+    writeFile(f, "")
+
+  try:
+    withCache()
+    global()
+  finally:
+    for f in files:
+      removeFile(f)
+
+# Omit testing at compile time, which errors due to `getcwd`.
+main()
