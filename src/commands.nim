4a7cdb6bf8ac90135c8e4d40c1ed20ff7519714b
numeral <numerical@gmail.com>
2024-09-14 09:57:21 -0400

    feat: add daemon command/mode
    Adds a new base command; daemon
    Allows chalk to report over time (similar to exec)
    but without requiring a binary for chalk to exec
    
    Currently very slim but can be used as a jumping point
    to add c&c for other chalk instances

--- src/commands.nim
+++ src/commands.nim
@@ -10,6 +10,6 @@
 
 import commands/[cmd_insert, cmd_extract, cmd_delete, cmd_docker, cmd_exec,
                  cmd_env, cmd_dump, cmd_load, cmd_version, cmd_setup, cmd_help,
-                 cmd_internal]
+                 cmd_internal, cmd_daemon]
 export cmd_insert, cmd_extract, cmd_delete, cmd_docker, cmd_exec, cmd_env,
-       cmd_dump, cmd_load, cmd_version, cmd_setup, cmd_help, cmd_internal
+       cmd_dump, cmd_load, cmd_version, cmd_setup, cmd_help, cmd_internal, cmd_daemon
---
+++ src/commands/cmd_daemon.nim
@@ -0,0 +1,47 @@
+##
+## Copyright (c) 2023, Crash Override, Inc.
+##
+## This file is part of Chalk
+## (see https://crashoverride.com/docs/chalk)
+##
+
+##
+## chalk `daemon` command
+##
+
+import os, std/posix
+import ".."/[config, util, reporting]
+
+# this const is not available in nim stdlib hence manual c import
+var TIOCNOTTY {.importc, header: "sys/ioctl.h"}: cuint
+
+proc daemon() =
+  while true:
+    doReporting()
+    sleep 5000
+
+proc runCmdDaemon*() =
+  let
+    period = int(get[Con4mDuration](getChalkScope(), "daemon.period"))
+
+  let pid = fork()
+  if pid == -1:
+    error("Could not spawn daemon process.")
+    setExitCode(1)
+  if pid != 0:
+    quit()
+
+  let is_err = setpgid(0, 0)
+  if is_err == -1:
+    setExitCode(1)
+    error("Chalk couldn't reset the process group: $1" % [$strerror(errno)])
+    quit()
+
+  if isatty(0) != 0:
+    let is_err = ioctl(0, TIOCNOTTY)
+    if is_err == -1:
+      setExitCode(1)
+      error("Error on disconnecting from tty: $1" % [$strerror(errno)])
+
+  # loop forever
+  daemon()
