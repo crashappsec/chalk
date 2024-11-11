fc8048a2c2b40d919d8d74e44790f50bd0f02673
Miroslav Shubernetskiy <miroslav@miki725.com>
2024-11-11 11:17:53 -0500

    fix: embed attestation can lookup chalk keys in multiple paths
    Previously embed provider only looked up keys in a single location
    which by default was current working directly.
    This is not desirable in CI as placing additional files in PWD
    can have other side-effects.
    As such setup-chalk-action was placing the generated key relative
    to chalk binary however chalk was not able to lookup that key.

--- src/attestation/embed.nim
+++ src/attestation/embed.nim
@@ -9,17 +9,19 @@ import ".."/[chalk_common, config]
 import "."/utils
 
 type Embed = ref object of AttestationKeyProvider
-  location: string
+  filename:  string
+  save_path: string
+  get_paths: seq[string]
 
 proc initCallback(this: AttestationKeyProvider) =
-  let
-    self        = Embed(this)
-    location    = attrGet[string]("attestation.attestation_key_embed.location")
-  self.location = location
+  let self = Embed(this)
+  self.filename  = attrGet[string]("attestation.attestation_key_embed.filename")
+  self.save_path = attrGet[string]("attestation.attestation_key_embed.save_path")
+  self.get_paths = attrGet[seq[string]]("attestation.attestation_key_embed.get_paths")
 
 proc generateKeyCallback(this: AttestationKeyProvider): AttestationKey =
   let self = Embed(this)
-  result = mintCosignKey(self.location)
+  result = mintCosignKey(self.save_path.joinPath(self.filename))
   echo()
   echo("------------------------------------------")
   echo("CHALK_PASSWORD=", result.password)
@@ -29,9 +31,20 @@ to provide it via CHALK_PASSWORD environment variable.
 """)
 
 proc retrieveKeyCallback(this: AttestationKeyProvider): AttestationKey =
-  let self = Embed(this)
-  result = getCosignKeyFromDisk(self.location)
-  info("Loaded existing attestation keys from: " & self.location)
+  let
+    self      = Embed(this)
+    (dir, _)  = getMyAppPath().splitPath()
+    password  = getChalkPassword()
+  var lastError = "no paths to lookup key"
+  for i in self.get_paths:
+    let path = i.replace("$CHALK", dir).joinPath(self.filename)
+    try:
+      result = getCosignKeyFromDisk(path, password = password)
+      info("Loaded existing attestation keys from: " & path)
+      return
+    except:
+      lastError = getCurrentExceptionMsg()
+  raise newException(ValueError, "Could not retrieve key: " & lastError)
 
 proc retrievePasswordCallback(this: AttestationKeyProvider, key: AttestationKey): string =
   return getChalkPassword()
