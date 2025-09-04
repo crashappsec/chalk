94df9b1bfebf3525d1a1f8457197e55ee1e031b5
Miroslav Shubernetskiy <miroslav@miki725.com>
2025-09-04 15:05:45 -0400

    test: local test for the dust script
    also fixed some minor issues like in main loop make_api_request wasnt
    handling errors properly

--- tests/functional/server/app.py
+++ tests/functional/server/app.py
@@ -1,4 +1,16 @@
-from fastapi import FastAPI
+# Copyright (c) 2023-2025, Crash Override, Inc.
+#
+# This file is part of Chalk
+# (see https://crashoverride.com/docs/chalk)
+import uuid
+import time
+from typing import Annotated
+
+from fastapi import (
+    FastAPI,
+    Response,
+    Header,
+)
 
 
 app = FastAPI()
@@ -7,3 +19,31 @@ app = FastAPI()
 @app.get("/health")
 def health():
     return
+
+
+extensions: dict[str, int] = {}
+
+
+@app.post("/2020-01-01/extension/register")
+async def register(response: Response):
+    extension_id = str(uuid.uuid4())
+    extensions[extension_id] = 0
+    response.headers["Lambda-Extension-Identifier"] = extension_id
+    return {}
+
+
+@app.get("/2020-01-01/extension/event/next")
+async def next(lambda_extension_identifier: Annotated[str, Header()]):
+    time.sleep(1)
+    extensions[lambda_extension_identifier] += 1
+    print(extensions)
+    if extensions[lambda_extension_identifier] < 5:
+        return {"eventType": "INVOKE"}
+    else:
+        return {"eventType": "SHUTDOWN"}
+
+
+@app.post("/2020-01-01/extension/exit")
+async def exit(lambda_extension_identifier: Annotated[str, Header()]):
+    extensions.pop(lambda_extension_identifier, None)
+    return {}
